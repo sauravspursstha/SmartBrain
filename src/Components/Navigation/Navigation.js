d710d90e3697eb9d49a0d020352811e966e38b8c
Saurav Shrestha <sauravspursstha@gmail.com>
2018-08-02 20:45:39 +0545

    Done with the front end of core part of the app

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Navigation = () => {
+return(
+    <nav style = {{display:'flex', justifyContent:'flex-end', paddingRight:'5px'}}>
+    <p className='f3 link dim white underline pa1 pointer'> Logout </p>
+    </nav>
+
+)
+}
+
+export default Navigation;
