c514a43f08c305410f2a80b94d828ec105aafa01
Saurav Shrestha <sauravspursstha@gmail.com>
2018-08-20 10:46:00 +0545

    Multiple face detection

--- src/Components/FaceRecognition/FaceRecognition.js
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -8,7 +8,7 @@ return(
    <div className='absolute mt2'>
    <img id="inputImage" alt = '' src={image} width='500px' heigh='auto' />
    <div className = "bounding-box" style ={{top: box.topRow, right: box.rightCol,  bottom: box.bottomRow, left: box.leftCol}}>
-  
+   
   </div>
   </div>
 
